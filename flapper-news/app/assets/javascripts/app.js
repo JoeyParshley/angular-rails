b7b14533089aef443b48522f48a6c5c9888dddbb
Joey Parshley <jparshle@akamai.com>
2018-04-06 14:41:26 -0400

    this is my branch that was not quite workgin

--- flapper-news/app/assets/javascripts/app.js
+++ flapper-news/app/assets/javascripts/app.js
@@ -4,12 +4,16 @@ angular.module('flapperNews', ['ui.router','templates'])
 	'$stateProvider',
 	'$urlRouterProvider',
 	function($stateProvider, $urlRouterProvider) {
-
 	  $stateProvider
 	    .state('home', {
 	      url: '/home',
 	      templateUrl: 'home/_home.html',
-	      controller: 'MainCtrl'
+	      resolve: {
+	      	postPromise: ['posts', function(posts){
+	      		return posts.getAll();
+	      	}]
+	      },
+	      controller: 'MainCtrl',
 	    })
 	    .state('posts', {
 		  url: '/posts/{id}',
--- flapper-news/app/assets/javascripts/posts/posts.js
+++ flapper-news/app/assets/javascripts/posts/posts.js
@@ -1,13 +1,27 @@
 angular.module('flapperNews')
-.factory('posts', [function(){
+.factory('posts', [
+'$http',
+function($http){
   var o = {
-    posts: [
-		{title: 'post 1', upvotes: 5},
-		{title: 'post 2', upvotes: 2},
-		{title: 'post 3', upvotes: 15},
-		{title: 'post 4', upvotes: 9},
-		{title: 'post 5', upvotes: 4}
-	]
+    posts: []
+    // {title: 'post 1', upvotes: 5,comments:[]},
+    // {title: 'post 2', upvotes: 2,comments:[]},
+    // {title: 'post 3', upvotes: 15,comments:[]},
+    // {title: 'post 4', upvotes: 9,comments:[]},
+    // {title: 'post 5', upvotes: 4,comments:[]}]
   };
+  
+  o.getAll = function() {
+    return $http.get('/posts.json').then(function(data){
+      angular.copy(data, o.posts);
+    });
+  };
+
+  o.create = function(post) {
+    return $http.post('/posts.json', post).then(function(data){
+      o.posts.push(data);
+    });
+  };
+
   return o;
 }])
